657076e0032d28cc2bb2ec847ef8f2794cf57434
lopo <lopo971020@icloud.com>
2023-11-23 21:29:43 +0800

    alibaba nacos 学习

--- order-service/src/main/java/com/lopo/service/impl/OrderServiceImpl.java
+++ order-service/src/main/java/com/lopo/service/impl/OrderServiceImpl.java
@@ -5,14 +5,19 @@ import com.lopo.domain.User;
 import com.lopo.mapper.OrderMapper;
 import com.lopo.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+import java.util.Random;
 
 @Service
 public class OrderServiceImpl implements OrderService {
     private OrderMapper orderMapper;
     private RestTemplate restTemplate;
-
+    private DiscoveryClient discoveryClient;
     @Autowired
     public void setOrderMapper(OrderMapper orderMapper) {
         this.orderMapper = orderMapper;
@@ -23,10 +28,19 @@ public class OrderServiceImpl implements OrderService {
         this.restTemplate = restTemplate;
     }
 
+    @Autowired
+    public void setDiscoveryClient(DiscoveryClient discoveryClient) {
+        this.discoveryClient = discoveryClient;
+    }
+
     @Override
     public Order getById(Integer orderId) {
         Order order = orderMapper.selectById(orderId);
-        String url = "http://userService:8082/user/" + order.getUserId();
+
+        List<ServiceInstance> instances = discoveryClient.getInstances("userService");
+        Random random = new Random();
+        ServiceInstance serviceInstance = instances.get(random.nextInt(instances.size()));
+        String url = serviceInstance.getUri() +"/user/" + order.getUserId();
         User user = restTemplate.getForObject(url, User.class);
         order.setUser(user);
         return order;
